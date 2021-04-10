c31fd2f7fdde5d708243ef435910eb3131ddb03e
Christopher Marland <christopher.marland@theinformationlab.co.uk>
2021-04-10 14:47:45 +0100

    Add script for collecting datasource IDs

---
+++ tabserverdatasourceids.py
@@ -0,0 +1,26 @@
+from ayx import Alteryx
+import pandas as pd
+import numpy as np
+import tableauserverclient as TSC
+
+tableau_auth = TSC.PersonalAccessTokenAuth('', ', site_id=’’)
+
+server = TSC.Server('https://scv-tableau.live.tools.bbc.co.uk/', use_server_version=True)
+
+server.auth.sign_in(tableau_auth)
+
+with server.auth.sign_in(tableau_auth):
+    request_options = TSC.RequestOptions(pagesize=1000)
+    all_datasources = list(TSC.Pager(server.datasources))
+    index = 0
+    datasources = {}
+    for ds in all_datasources:
+        datasources[index] = [ds.name, ds.id]
+        index +=1
+        print(datasources)
+       
+df = pd.DataFrame.from_dict(datasources, orient='index')
+
+df.rename({0:'Datasource_Name', 1:'Datasource_ID'}, axis='columns', inplace=True)
+
+Alteryx.write(df, 1)
